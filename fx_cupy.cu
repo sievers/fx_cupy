cbf40c0a597d5a71fe144a02010f9c820639b5f9
sievers <jonathan.sievers@mcgill.ca>
2024-02-03 21:42:54 -0500

    wrote some kernels to handle small matrix stuff/pfb convolution better

---
+++ fx_cupy.cu
@@ -0,0 +1,174 @@
+//nvcc -o libfx_cupy.so fx_cupy.cu -shared -Xcompiler -fPIC -lgomp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cufft.h>
+#include <omp.h>
+#include <cuComplex.h>
+
+
+#define KMAX 4
+
+extern "C"
+{
+void h2d(void *hptr, void *dptr, int nbyte)
+{
+  if (cudaMemcpy(dptr,hptr,(size_t)nbyte,cudaMemcpyHostToDevice)!=cudaSuccess)
+    fprintf(stderr,"Failed copy on h2d with %d bytes.\n",nbyte);
+}
+}
+
+/*--------------------------------------------------------------------------------*/
+template<typename T>
+__global__
+void conv_cols_gpu(T *in, float *out, float *win, int n, int m, int kk)
+//__global__
+//void conv_cols_gpu(int8_t *in, float *out, float *win, int n, int m, int kk)
+
+{
+  float mywin[KMAX];
+  float mydat[KMAX];
+  int di=blockDim.x*gridDim.x;
+  for (int i=blockDim.x*blockIdx.x+threadIdx.x;i<n;i+=di)
+    {
+      //copy window
+      for (int j=0;j<kk;j++)
+	mywin[j]=win[i+j*n];
+      //copy first bit of data
+      for (int j=0;j<kk-1;j++)
+	mydat[j]=in[i+j*n];
+
+      //loop over rows
+      for (int j=kk-1;j<m;j++) {
+	mydat[kk-1]=in[i+(j)*n];
+	float tmp=0;
+	for (int k=0;k<kk;k++) 
+	  tmp+=mywin[k]*mydat[k];
+	//out[i+(j+1-kk)*n]=mydat[0];
+	out[i+(j+1-kk)*n]=tmp;
+	//out[i+(j+1-kk)*n]=mywin[1];
+	for (int k=0;k<kk-1;k++)
+	  mydat[k]=mydat[k+1];
+
+	
+      }
+      
+    }
+}
+/*--------------------------------------------------------------------------------*/
+extern "C"
+{
+void conv_cols(void *in, float *out, float *win, int n, int m, int kk, int elemsize)
+{
+  //printf("elemsize is %d\n",elemsize);
+  switch(elemsize) {
+  case 1:
+    conv_cols_gpu<<<128,256>>>((int8_t *)in,out,win,n,m,kk);
+    break;
+  case 2:
+    conv_cols_gpu<<<256,256>>>((int16_t *)in,out,win,n,m,kk);
+    break;
+  case 4:
+    conv_cols_gpu<<<256,256>>>((int *)in,out,win,n,m,kk);
+    break;
+  case -4:
+    conv_cols_gpu<<<256,256>>>((float *)in,out,win,n,m,kk);
+    break;
+  default:
+    fprintf(stderr,"Unhandled element size %d in conv_cols_gpu.\n",elemsize);
+    break;
+  }
+}
+}
+/*--------------------------------------------------------------------------------*/
+
+#define BS_CHERK 8
+
+__global__
+void cherk_batched_gpu(cuFloatComplex *in, cuFloatComplex *out, int nmat, int n, int k)
+{
+  int nblock=k/BS_CHERK;
+  if ((nblock*BS_CHERK)<n)
+    nblock++;
+  for (int imat=blockIdx.x;imat<nmat;imat+=gridDim.x)
+    {
+      __shared__ cuFloatComplex patch[BS_CHERK][BS_CHERK];
+
+      cuFloatComplex tmp=make_cuFloatComplex(0,0);
+      for (int i=0;i<nblock;i++) {
+	int myind=i*BS_CHERK+threadIdx.x;
+	if (myind<k)
+	  patch[threadIdx.y][threadIdx.x]=in[threadIdx.y*k+myind+imat*n*k];
+	else
+	  patch[threadIdx.y][threadIdx.x]=make_cuFloatComplex(0,0);
+	__syncthreads();
+	for (int j=0;j<BS_CHERK;j++)
+	  tmp=cuCaddf(tmp,cuCmulf(patch[threadIdx.x][j],cuConjf(patch[threadIdx.y][j])));
+	__syncthreads();
+	//tmp=patch[0][0];
+	//__syncthreads();
+      }
+      out[imat*n*n+threadIdx.x*BS_CHERK+threadIdx.y]=tmp;
+      //out[imat*n*n+threadIdx.x*BS_CHERK+threadIdx.y]=make_cuFloatComplex(1,0);
+      __syncthreads();
+      //out[0]=make_cuFloatComplex(1,0);
+      
+    }
+}
+/*--------------------------------------------------------------------------------*/
+__global__
+void cherk_batched_gpu_transpose(cuFloatComplex *in, cuFloatComplex *out, int nmat, int n, int k)
+///do a batched cherk, but expect data ordering as per PFB, so (n,k,nmat)
+{
+  int nblock=k/BS_CHERK;
+  if ((nblock*BS_CHERK)<n)
+    nblock++;
+  for (int imat=blockIdx.x;imat<nmat;imat+=gridDim.x)
+    {
+      __shared__ cuFloatComplex patch[BS_CHERK][BS_CHERK];
+
+      cuFloatComplex tmp=make_cuFloatComplex(0,0);
+      for (int i=0;i<nblock;i++) {
+	
+	int myind=i*BS_CHERK+threadIdx.x;
+	if (myind<k)
+	  patch[threadIdx.y][threadIdx.x]=in[threadIdx.y*k+myind+imat*n*k];
+	else
+	  patch[threadIdx.y][threadIdx.x]=make_cuFloatComplex(0,0);
+
+	__syncthreads();
+	for (int j=0;j<BS_CHERK;j++)
+	  tmp=cuCaddf(tmp,cuCmulf(patch[threadIdx.x][j],cuConjf(patch[threadIdx.y][j])));
+	__syncthreads();
+	//tmp=patch[0][0];
+	//__syncthreads();
+      }
+      out[imat*n*n+threadIdx.x*BS_CHERK+threadIdx.y]=tmp;
+      //out[imat*n*n+threadIdx.x*BS_CHERK+threadIdx.y]=make_cuFloatComplex(1,0);
+      __syncthreads();
+      //out[0]=make_cuFloatComplex(1,0);
+      
+    }
+}
+/*--------------------------------------------------------------------------------*/
+
+extern "C"
+{
+void cherk_batched(cuFloatComplex *in, cuFloatComplex *out, int nmat, int n, int k)
+{
+  //cherk_batched_gpu<<<512,(BS_CHERK,BS_CHERK)>>>(in,out,nmat,n,k);
+  dim3 threadsize;
+  threadsize.x=BS_CHERK;
+  threadsize.y=BS_CHERK;
+  threadsize.z=1;
+  cherk_batched_gpu<<<256,threadsize>>>(in,out,nmat,n,k);
+}
+}
+/*--------------------------------------------------------------------------------*/
+__global__
+void apply_pfb_win(int8_t *din, float *dout, float *win, int ntap, int nchan, int npol, int nblock, int nn)
+{
+  
+}
+
--- fx_cupy.py
+++ fx_cupy.py
@@ -1,7 +1,66 @@
 import numpy as np
 import cupy as cp
+from cupyx.profiler import benchmark
+import cupyx
 import time
+import ctypes
 
+mylib=ctypes.cdll.LoadLibrary("libfx_cupy.so")
+conv_cols_gpu=mylib.conv_cols
+conv_cols_gpu.argtypes=(ctypes.c_void_p,ctypes.c_void_p,ctypes.c_void_p,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int)
+cherk_batched_gpu=mylib.cherk_batched
+cherk_batched_gpu.argtypes=(ctypes.c_void_p,ctypes.c_void_p,ctypes.c_int,ctypes.c_int,ctypes.c_int)
+
+
+def conv_cols(din,win):
+    n=din.shape[1]
+    m=din.shape[0]
+    k=win.shape[0]
+
+    itemsize=0
+    if din.dtype=='int8':
+        itemsize=1
+    if din.dtype=='int16':
+        itemsize=2
+    if din.dtype=='int32':
+        itemsize=4
+    if din.dtype=='float32':
+        itemsize=-4
+    assert(not(itemsize==0))
+    out=cp.empty([m,n],dtype='float32')
+    conv_cols_gpu(din.data.ptr,out.data.ptr,win.data.ptr,n,m,k,itemsize)
+    return out
+
+def cherk_batched(mat):
+    out=cp.empty([mat.shape[0],mat.shape[1],mat.shape[1]],dtype='complex64')
+    cherk_batched_gpu(mat.data.ptr,out.data.ptr,mat.shape[0],mat.shape[1],mat.shape[2])
+    return out
+    
+
+
+def cast_pfb(dat,win,ntap,nchan):
+    """Apply the window function/adding part of a PFB while including a cast
+    to float32.  Since I was too lazy to handle the logic for multiple antennas,
+    the final ntap-1 blocks will be garbage.  Rather than zero them here, I plan
+    to ignore those blocks when doing the correlation."""
+    nblock=dat.shape[1]//(2*nchan)
+    nant=dat.shape[0]
+    dd=cp.reshape(dat,[nant*nblock,2*nchan])
+    ww=cp.reshape(win,[ntap,2*nchan])
+    return cp.reshape(conv_cols(dd,ww),[nant,nblock,2*nchan])
+    
+
+def pfb_xcorr_block(dat,win,ntap,nchan):
+    dat_pfb=cast_pfb(dat,win,ntap,nchan)
+    dft=cp.fft.rfft(dat_pfb,axis=-1)
+
+    tmp=cp.transpose(dft,(2,0,1))
+    #print('tmp shape is ',tmp.shape)
+    tmp[:,:,-ntap+1:]=0
+    #prod=tmp@(cp.conj(cp.transpose(tmp,(0,2,1))))
+    prod=cherk_batched(tmp)
+    return prod
+    
 def reshape_cast(dat,nn,nblock):
     nsamp=nn*nblock
     nant=dat.shape[0]
@@ -29,6 +88,7 @@ dt=0.1
 nn=2*nchan
 nblock=int((dt*fsamp)/nn)
 nsamp=nblock*nn
+
 ntap=4
 win=get_win(nn*ntap)
 
@@ -37,20 +97,88 @@ try:
     assert(hdat.shape[0]==nfeed)
     assert(hdat.shape[1]==nsamp)
 except:
-    hdat=(3*np.random.randn(nfeed,nsamp)).astype('int8')
-
+    hdat=(3*np.random.randn(nfeed,nsamp)).astype('int16')
+    tmp=cupyx.empty_like_pinned(hdat)
+    tmp[:]=hdat
+    hdat=tmp
+    
 tot=0
 do_pfb=True
 
+
+
 for iter in range(10):
+    cp.cuda.runtime.deviceSynchronize()
     t1=time.time()
     ddat=cp.asarray(hdat)
-    dblocks=reshape_cast(ddat,nn,nblock)
-    if do_pfb:
-        dblocks=reshape_pfb(dblocks,win,ntap)
-    dft=cp.fft.rfft(dblocks,axis=-1)
-    tmp=cp.transpose(dft,(2,0,1))
-    prod=tmp@(cp.conj(cp.transpose(tmp,(0,2,1))))
-    tot=tot+cp.asnumpy(prod)
+    if False:
+        dblocks=reshape_cast(ddat,nn,nblock)
+        if do_pfb:
+            dblocks_org=dblocks
+            dblocks=reshape_pfb(dblocks_org,win,ntap)
+        dft=cp.fft.rfft(dblocks,axis=-1)
+        tmp=cp.transpose(dft,(2,0,1))
+        prod=tmp@(cp.conj(cp.transpose(tmp,(0,2,1))))
+        tot=tot+cp.asnumpy(prod)
+    else:
+        tot=tot+pfb_xcorr_block(ddat,win,ntap,nchan)
+    cp.cuda.runtime.deviceSynchronize()
     t2=time.time()
     print('iter time ',t2-t1)
+
+print('transfer: ',benchmark(cp.asarray,(hdat,),n_repeat=100))
+print('reshape_cast: ',benchmark(reshape_cast,(ddat,nn,nblock),n_repeat=100))
+ddat=cp.asarray(hdat)
+print('new pfb: ',benchmark(pfb_xcorr_block,(ddat,win,ntap,nchan),n_repeat=100))
+print('cherk_batched: ',benchmark(cherk_batched,(tmp,),n_repeat=100))
+
+niter=100
+
+cp.cuda.runtime.deviceSynchronize()
+t1=time.time()
+for i in range(niter):
+    dblocks=reshape_pfb(dblocks_org,win,ntap)
+cp.cuda.runtime.deviceSynchronize()
+t2=time.time()
+print('pfb: ',(t2-t1)/niter*1e3,' msec')
+
+cp.cuda.runtime.deviceSynchronize()
+t1=time.time()
+for i in range(niter):
+    dft=cp.fft.rfft(dblocks,axis=-1)
+cp.cuda.runtime.deviceSynchronize()
+t2=time.time()
+print('fft: ',(t2-t1)/niter*1e3,' msec')
+
+cp.cuda.runtime.deviceSynchronize()
+t1=time.time()
+for i in range(niter):
+    tmp=cp.transpose(dft,(2,0,1))
+cp.cuda.runtime.deviceSynchronize()
+t2=time.time()
+print('transpose: ',(t2-t1)/niter*1e3,' msec')
+
+cp.cuda.runtime.deviceSynchronize()
+t1=time.time()
+for i in range(niter):
+    prod=tmp@(cp.conj(cp.transpose(tmp,(0,2,1))))
+cp.cuda.runtime.deviceSynchronize()
+t2=time.time()
+print('xcorr: ',(t2-t1)/niter*1e3,' msec')
+
+
+tot=0
+cp.cuda.runtime.deviceSynchronize()
+t1=time.time()
+for i in range(niter):
+    tot=tot+cp.asnumpy(prod)
+cp.cuda.runtime.deviceSynchronize()
+t2=time.time()
+print('copy to host: ',(t2-t1)/niter*1e3,' msec')
+
+
+
+
+#print('pfb: ',benchmark(reshape_pfb,(dblocks_org,win,ntap)))
+#print('dft: ',benchmark(cp.fft.rfft,(dblocks,None,-1)))
+      
